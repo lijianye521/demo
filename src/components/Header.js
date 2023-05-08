83b2abf686e046f36d71cf41089307763f545180
lijianye521 <88470483+lijianye521@users.noreply.github.com>
2023-05-08 15:15:55 +0800

    commit version1.5

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Layout, Menu, Switch } from 'antd';
+import { Layout, Menu, Switch, Button } from 'antd';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useTheme } from '../App';
+import { MenuOutlined } from '@ant-design/icons';
 const { Header: AntHeader } = Layout;
 
 const HeaderComponent = () => {
@@ -34,35 +35,29 @@ const HeaderComponent = () => {
 
   return (
     <AntHeader>
-     <Menu
-  theme={theme}
-  mode="horizontal"
-  className={theme === 'dark' ? 'dark-theme' : 'light-theme'}
-  selectedKeys={getSelectedKeys()}
-  style={{ display: 'flex', justifyContent: 'space-between' }}
->
-
-        <div>
-          <Menu.Item key="1" onClick={() => window.location.href = '/'}>
+      <Menu
+        theme={theme}
+        mode="horizontal"
+        className={theme === 'dark' ? 'dark-theme' : 'light-theme'}
+        selectedKeys={getSelectedKeys()}
+        style={{ display: 'flex', justifyContent: 'space-between' }}
+      >
+        <div className="menu-left">
+          <Menu.Item key="1" onClick={() => navigate('/')}>
             Home
           </Menu.Item>
-          <Menu.Item key="6">
-            Home1
-          </Menu.Item>
-          <Menu.Item key="7">
-            Home2
-          </Menu.Item>
-          <Menu.Item key="8">
-            Home3
-          </Menu.Item>
-          <Menu.Item key="9">
-            Home4
-          </Menu.Item>
+          <Menu.Item key="6">Home1</Menu.Item>
+          <Menu.Item key="7">Home2</Menu.Item>
+          <Menu.Item key="8">Home3</Menu.Item>
+          <Menu.Item key="9">About us</Menu.Item>
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <Switch checked={theme === 'dark'} onChange={handleThemeChange} style={{ marginRight: '1rem' }} />
-
+        <div className="menu-right">
+          <Switch
+            checked={theme === 'dark'}
+            onChange={handleThemeChange}
+            style={{ marginRight: '1rem' }}
+          />
           {!user ? (
             <>
               <Menu.Item key="2" onClick={() => navigate('/login')}>
@@ -77,7 +72,6 @@ const HeaderComponent = () => {
               <Menu.Item key="4" onClick={handleLogout}>
                 Logout
               </Menu.Item>
-
               <Menu.Item key="5" disabled>
                 Welcome, {user.username}
               </Menu.Item>
@@ -85,6 +79,30 @@ const HeaderComponent = () => {
           )}
         </div>
       </Menu>
+      <style>{`
+        .menu-left {
+          display: flex;
+        }
+        .menu-right {
+          display: flex;
+          align-items: center;
+        }
+        @media (max-width: 768px) {
+          .menu-left,
+          .menu-right {
+            display: none;
+          }
+          .menu-button {
+            display: inline-block !important;
+          }
+          .ant-menu {
+            flex-direction: column;
+          }
+          .ant-menu-item {
+            text-align: center;
+          }
+        }
+      `}</style>
     </AntHeader>
   );
 };
